d152fafdc501ae6fd00c485ec3a13a2d7be119dd
Shuto Shikama <shuto@VH-AD3L>
2021-10-11 00:53:47 +0900

    [WIP] ヒープを使わないベジエの検証

--- Include/ut/detail/bezier_surface.hpp
+++ Include/ut/detail/bezier_surface.hpp
@@ -69,4 +69,61 @@ private:
   std::vector<Vertex> m_vertices;
 };
 
+namespace ut {namespace detail {
+
+template<uint8_t MaxDivX, uint8_t MaxDivY>
+class TBezierSurface
+{
+public:
+  //! @brief バーンスタイン多項式 の値を算出します。
+  //! @pre 0 ≦ t ≦ 1
+  //! @pre 0 ≦ i ≦ 3
+  static float ComputeBernsteinPolynormal(const float t, std::uint32_t i);
+
+  //! @brief ベジエ曲面の分割数を更新します。
+  //! @detail 分割数を 0 で更新すると四角メッシュを生成します。
+  //! @param[in] div 横方向の分割数
+  //! @param[in] subDiv 縦方向の分割数
+  //! @note 分割数の限界は未確認です。どこかでオーバーフローが発生します。
+  void update(const std::uint32_t div, std::uint32_t subDiv);
+
+  //! @brief制御点を取得します。
+  //! @detail
+  //! 制御点と UV 座標の関係は以下。
+  //! uv は [0, 1] の値をとります。
+  //!   → u
+  //! ↓  0---1---2---3
+  //! v  4---5---6---7
+  //!    8---9---10--11
+  //!    12--13--14--15
+  fj::Vector& GetControllPoint(const uint8_t index) noexcept
+  {
+    return m_ControllPoints[index];
+  }
+
+  const auto& GetVertices() const noexcept 
+  {
+      return &m_Vertices;
+  }
+
+  const auto& GetIndices() const noexcept   
+  {
+      return m_Indices; 
+  }
+
+private:
+  /* 制御点の順番はこんな感じ
+   0---1---2---3
+   4---5---6---7
+   8---9---10--11
+   12--13--14--15
+   */
+  std::array<fj::Vector, 16> m_ControllPoints = {};
+
+  std::array<uint32_t, 64> m_Indices = {};
+  std::array<fj::Vertex, 64> m_Vertices = {};
+};
+
+}}
+
 #endif /* bezier_surface_hpp */
